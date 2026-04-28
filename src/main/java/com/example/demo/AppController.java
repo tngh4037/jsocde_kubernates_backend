821dec019e2841fe29088f826146ab98d0f406b9
tngh4037 <tngh4037@gmail.com>
2026-04-29 02:31:23 +0900

    docs: 백엔드(Spring Boot) 서버에 환경변수 등록해 사용하기

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+    @Value("${MY_ACCOUNT:default}")
+    private String myAccount;
+
+    @Value("${MY_PASSWORD:default}")
+    private String myPassword;
+
     @GetMapping("/")
     public String home() {
-        return "Hello, World!";
+        return "myAccount: " + myAccount + ", myPassword: " + myPassword;
     }
+
 }
